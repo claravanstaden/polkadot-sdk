44730a0fda63e70251ab0ecc4bafe13b09c863cf
claravanstaden <claravanstaden64@gmail.com>
2025-04-09 10:22:04 +0200

    adds weight limit for system frontend calls

--- bridges/snowbridge/pallets/system-frontend/src/lib.rs
+++ bridges/snowbridge/pallets/system-frontend/src/lib.rs
@@ -206,7 +206,11 @@ pub mod pallet {
 			let dest = T::BridgeHubLocation::get();
 			let call =
 				Self::build_register_token_call(origin_location.clone(), asset_location, metadata)?;
-			let remote_xcm = Self::build_remote_xcm(&call);
+			let remote_xcm = Self::build_remote_xcm(
+				&call,
+				T::WeightInfo::register_token()
+					.saturating_add(T::BackendWeightInfo::transact_register_token()),
+			);
 			let message_id = Self::send_xcm(origin_location, dest.clone(), remote_xcm.clone())
 				.map_err(|error| Error::<T>::from(error))?;
 
@@ -251,10 +255,10 @@ pub mod pallet {
 			Ok(call)
 		}
 
-		fn build_remote_xcm(call: &impl Encode) -> Xcm<()> {
+		fn build_remote_xcm(call: &impl Encode, weight_limit: Weight) -> Xcm<()> {
 			Xcm(vec![
 				DescendOrigin(T::PalletLocation::get()),
-				UnpaidExecution { weight_limit: Unlimited, check_origin: None },
+				UnpaidExecution { weight_limit: Limited(weight_limit), check_origin: None },
 				Transact {
 					origin_kind: OriginKind::Xcm,
 					call: call.encode().into(),
